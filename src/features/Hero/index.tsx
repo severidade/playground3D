c4fc9066156a34d8cff5489ce6baba8f5ad116d8
severo <oliveira.severo@gmail.com>
2025-11-18 10:23:12 -0300

    feat: :rocket: Cria scroll_cta para navegar para a próxima seção

--- src/features/Hero/index.tsx
+++ src/features/Hero/index.tsx
@@ -1,14 +1,32 @@
+import gsap from 'gsap';
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
+
+import { useLayoutEffect } from 'react';
 import styles from './hero.module.css';
 
 function Hero() {
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollToPlugin);
+  });
   return (
     <section id="hero">
       <div className={styles.container_hero}>
         <div id="text-1" className={styles.hero_text_block}>
           <h1 className={styles.hero_title}>MacBook Pro M4</h1>
-          <p className="subtitle">
+          <a
+            onClick={(e) => {
+              e.preventDefault();
+              gsap.to(window, {
+                duration: 1.7,
+                scrollTo: '#section1',
+                ease: 'power4.inOut',
+              });
+            }}
+            href="#section1"
+            className={styles.hero_scroll_cta}
+          >
             Desperte o seu próximo nível.
-          </p>
+          </a>
         </div>
       </div>
     </section>
