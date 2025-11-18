0734bea9425ba03dd9deca5f96404df7db10772e
severo <oliveira.severo@gmail.com>
2025-11-18 09:34:51 -0300

    refactor: :rocket: Remove target de animação

--- src/animations/TextScrollControl.tsx
+++ src/animations/TextScrollControl.tsx
@@ -1,24 +1,23 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
 function TextScrollControl() {
-  useEffect(() => {
+  useLayoutEffect(() => {
     const text1 = document.getElementById('text-1');
     const text2 = document.getElementById('text-2');
     const text3 = document.getElementById('text-3');
-    const subtitle = document.querySelector('.subtitle');
 
-    if (!text1 || !text2 || !text3 || !subtitle) return;
+    if (!text1 || !text2 || !text3) return;
 
     gsap
       .timeline({
         scrollTrigger: {
-          trigger: '#section1', /* será  uma prop 8 */
+          trigger: '#section1',
           start: 'top bottom',
-          end: '30% bottom',
+          end: '50% bottom',
           scrub: true,
         },
       })
